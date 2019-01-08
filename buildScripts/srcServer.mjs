760ba8bc000abd0c177c86851f54852b40bf9ab9
Andreas Spannagel <spannagel.andreas@gmail.com>
2019-01-08 03:11:13 +0100

    add basic webpack configuration.

--- buildScripts/srcServer.mjs
+++ buildScripts/srcServer.mjs
@@ -1,15 +1,26 @@
 import express from 'express'
-import { dirname, join } from 'path'
+import path from 'path'
 import open from 'open'
+import webpack from 'webpack'
+import middleware from 'webpack-dev-middleware'
+import config from '../webpack.config.dev.mjs'
 
 // Workaround for lack of __dirname in ES6 modules
-const __dirname = dirname(new URL(import.meta.url).pathname)
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 const port = 3000
 const app = express()
+const compiler = webpack(config)
+
+app.use(
+  middleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+)
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '../src/index.html'))
+  res.sendFile(path.join(__dirname, '../src/index.html'))
 })
 
 app.listen(port, err => {
---
+++ webpack.config.dev.mjs
@@ -0,0 +1,15 @@
+import path from 'path'
+
+// Workaround for lack of __dirname in ES6 modules
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+export default {
+  devtool: 'inline-source-map',
+  // entry: [path.resolve(__dirname, 'src/index')],
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+}
